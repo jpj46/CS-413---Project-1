5d399f7950d1eeb3790c5eaebe71fb59b6fa1d2c
jpj46 <48301533+jpj46@users.noreply.github.com>
2019-09-19 12:31:19 -0700

    made spawn times truly random, moved assets into a folder for organization

--- game.js
+++ game.js
@@ -5,13 +5,12 @@ gameport.appendChild( renderer.view );
 // Add containers and sprites
 var stage = new PIXI.Container();
 var background = new PIXI.Container();
-var sun = new PIXI.Sprite( PIXI.Texture.fromImage( "Sun.png" ) );
-var trees = new PIXI.Sprite( PIXI.Texture.fromImage( "Trees.png" ) );
-var sun = new PIXI.Sprite( PIXI.Texture.fromImage( "Sun.png" ) );
-var ground = new PIXI.Sprite( PIXI.Texture.fromImage( "ground.png" ) );
-var cloud1 = new PIXI.Sprite( PIXI.Texture.fromImage( "cloud1.png" ) );
-var cloud2 = new PIXI.Sprite( PIXI.Texture.fromImage( "cloud2.png" ) );
-var backdrop = new PIXI.Sprite( PIXI.Texture.fromImage( "Background.png" ) );
+var sun = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/Sun.png" ) );
+var trees = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/Trees.png" ) );
+var ground = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/ground.png" ) );
+var cloud1 = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/cloud1.png" ) );
+var cloud2 = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/cloud2.png" ) );
+var backdrop = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/Background.png" ) );
 
 // Add background to stage
 stage.addChild(background);
@@ -37,14 +36,15 @@ background.addChild(cloud2);
 // Add score text and score counter
 scoreCounter = 0;
 let scoreText = new PIXI.Text('Score: ' + scoreCounter, 
-      {fontFamily : 'Calibri', fontSize: 25, fill : 0x525252, align : 'center'});
+      {fontFamily : 'Calibri', fontSize: 25, fill : 0x000000, align : 'center'});
 stage.addChild( scoreText );
 
 // Animate the sprite and spawn the nanas!
 animate();
+bananaRandomizer();
 createjs.Tween.get( cloud1.position ).to( { x: 640 }, 100000);
 createjs.Tween.get( cloud2.position ).to( { x: -640 }, 100000);
-var bananaSpawner = setInterval( spawnBanana, Math.floor( Math.random() * 3000 ) );
+
 
 // Animates the stage
 function animate() 
@@ -57,28 +57,36 @@ function animate()
    //document.addEventListener( 'keydown', keyPressEventHandler );
 }
 
+// Randomizes spawn time of bananas
+function bananaRandomizer()
+{
+   var randomSpawnTime = Math.floor( Math.random() * 2000 );
+   setTimeout( function() { spawnBanana(); bananaRandomizer() }, randomSpawnTime );
+}
+
+// Handles mouse events
 function mouseHandler( moving_banana )
 {
    scoreCounter++;
-   stage.removeChild( moving_banana );
+   background.removeChild( moving_banana );
    //document.getElementById('coord').innerHTML = ("Click X: " + click.x + " Click Y: " + click.y);
 }
 
+// Spawns bananas into game, despawns them after a certain time
 function spawnBanana()
 {
-   var moving_banana = new PIXI.Sprite( PIXI.Texture.fromImage( "Banana.png" ) );
-
-   
+   var moving_banana = new PIXI.Sprite( PIXI.Texture.fromImage( "assets/Banana.png" ) );
    background.addChild( moving_banana );
    moving_banana.interactive = true;
    moving_banana.on( 'mousedown', function() { mouseHandler( moving_banana ) } );
    moving_banana.x = Math.floor( Math.random() * 600 );
    moving_banana.y = 25;
-   var rand_x = Math.floor( moving_banana.x + ( Math.random() * 50 ) - ( Math.random() * 50 ) );
-   createjs.Tween.get( moving_banana.position ).to( { y: 700 }, 5000);
+   var rand_x = Math.floor( moving_banana.x + ( Math.random() * 200 ) - ( Math.random() * 200 ) );
+   createjs.Tween.get( moving_banana.position ).to( { x: rand_x, y: 700 }, 5000 );
    setInterval( function() { checkUnclickedBanana( moving_banana ) }, 8000);
 }
 
+// If banana is unclicked after moving off screen, remove from game
 function checkUnclickedBanana( moving_banana )
 {
    stage.removeChild( moving_banana );
